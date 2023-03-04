5328894ab8da2632d7c6c8829823e4f1ed53344a
muratmnz <minazzmurat@gmail.com>
2023-03-04 19:10:14 +0300

    Initial Commit

---
+++ DiceApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  DiceApp
+//
+//  Created by Murat Minaz on 4.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //IBOutlet allows me to reference to UI elements
+    //connection codes with elements in UI
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // WHO         WHAT      VALUE
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        
+       // diceImageView2.alpha = 0.5
+    }
+
+
+}
+
