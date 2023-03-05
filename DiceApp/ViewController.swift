8db17b084a7c7c0f8aaae63969cf60fd586d60c8
muratmnz <minazzmurat@gmail.com>
2023-03-05 12:59:15 +0300

    feature: added dices and randomize dice

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -14,19 +14,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    var diceArray = ["DiceOne","DiceTwo","DiceThree","DiceFour","DiceFive","DiceSix"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         // WHO         WHAT      VALUE
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        //diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
         
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        //diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
         
        // diceImageView2.alpha = 0.5
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        
+        diceImageView1.image = UIImage(imageLiteralResourceName: diceArray.randomElement()!)
+            diceImageView2.image = UIImage(imageLiteralResourceName: diceArray.randomElement()!)
+            print(diceArray.randomElement()!)
+        
     }
     
 }
